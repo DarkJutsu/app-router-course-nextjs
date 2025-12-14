1c7b0366f3c9f4865ac162e12a1b083f4f7e9180
DarkJutsu <ulloajose34@gmail.com>
2025-12-14 16:49:52 -0600

    feat(app/dashboard):implement Suspense for latestInvoices

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -1,14 +1,13 @@
 import {lusitana} from "@/app/ui/fonts";
-import {fetchCardData, fetchLatestInvoices} from "@/app/lib/data";
+import {fetchCardData} from "@/app/lib/data";
 import RevenueChart from "@/app/ui/dashboard/revenue-chart";
-import LatestInvoices from "@/app/ui/dashboard/latest-invoices";
 import {Card} from "@/app/ui/dashboard/cards";
 import {Suspense} from "react";
-import {RevenueChartSkeleton} from "@/app/ui/skeletons";
+import {LatestInvoicesSkeleton, RevenueChartSkeleton} from "@/app/ui/skeletons";
+import LatestInvoices from "@/app/ui/dashboard/latest-invoices";
 
 export default async function Page() {
 
-  const latestInvoices = await fetchLatestInvoices()
   const {totalPaidInvoices, totalPendingInvoices, numberOfInvoices, numberOfCustomers} = await fetchCardData()
   return (
     <main>
@@ -23,7 +22,9 @@ export default async function Page() {
         <Suspense fallback={<RevenueChartSkeleton/>}>
           <RevenueChart/>
         </Suspense>
-        <LatestInvoices latestInvoices={latestInvoices}/>
+        <Suspense fallback={<LatestInvoicesSkeleton/>}>
+          <LatestInvoices/>
+        </Suspense>
       </div>
     </main>
   )
